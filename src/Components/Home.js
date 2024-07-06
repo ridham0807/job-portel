9bd39fd07deece20ee6529e935d8c3f76532b8f8
dharm <dpp6402@gmail.com>
2024-07-06 12:47:06 +0530

    home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      className="px-4 py-5 px-md-5 text-center text-lg-start"
+      style={{ backgroundColor: "hsl(0, 0%, 96%)" }}
+    >
+      <div className="container-home">
+        <div className="row-home gx-lg-5 align-items-center">
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <h1 className="my-3 display-3 fw-bold ls-tight">
+              Discover the Path
+              <br />
+              to Your
+              <br />
+              <span className="text" style={{ color: "#725AC1" }}>
+                Perfect Career Opportunity
+              </span>
+            </h1>
+            <p style={{ color: "hsl(217, 10%, 50.8%)" }}>
+              2400 Peoples are daily search in this portal, 100 user added job
+              portal!
+            </p>
+            <div className="input-group mt-3">
+              <div className="search">
+                <input placeholder="Search for jobs" type="text" />
+                <button type="submit">Search</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 img">
+            <img
+              src={require("./4153553.jpg")} 
+              alt="Career Search"
+              style={{ objectFit: "cover", width: "100%", height: "auto" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -32,48 +32,50 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
+          
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/business">
-                Post-jobs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/entertainment">
-                View-jobs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/health">
-                Posted Jobs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/science">
-                Dashboard
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/sports">
-                CONTECT
-              </Link>
-            </li>
-          </ul>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Post-jobs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  View-jobs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Posted Jobs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Contect
+                </Link>
+              </li>
+            </ul>
+        
         </div>
-        <div className="btnn">
-          <button className="btn " onClick={handleSignClick}>
-            LOGIN
-          </button>
-          <button className="btn " onClick={handleLoginClick}>
-            REGISTER
-          </button>
-        </div>
+          <div className="btnn">
+            <button className="btn " onClick={handleSignClick}>
+              LOGIN
+            </button>
+            <button className="btn " onClick={handleLoginClick}>
+              REGISTER
+            </button>
+          </div>
       </div>
     </nav>
   );
